d5086bf3cebfafd9e7c7ff880ee2d2b4023947cc
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2023-05-04 16:10:36 +0800

    Add Expense Form
    feat:
    - Add expenses form

---
+++ frontend/src/assets/dummyData.ts
@@ -0,0 +1,19 @@
+// Define an array of payment methods
+export const paymentMethods: string[] = [
+  "Credit Card",
+  "Debit Card",
+  "Cash",
+  "PayPal",
+  "Venmo",
+];
+
+// Define an array of expense categories
+export const expenseCategories: string[] = [
+  "Food",
+  "Transportation",
+  "Housing",
+  "Utilities",
+  "Entertainment",
+  "Healthcare",
+  "Education",
+];
