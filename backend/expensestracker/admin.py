b6a360b83b0d862457c23559dee9172f55ec0b00
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2023-05-06 17:49:07 +0800

    Remove django file
    remove django filw

--- backend/expensestracker/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import DailyExpense
-# add table to admin panel in db
-admin.site.register(DailyExpense)
--- backend/expensestracker/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import DailyExpense
-
-class ExpenseSerializer(ModelSerializer):
-    class Meta:
-        model = DailyExpense
-        fields = '__all__'
