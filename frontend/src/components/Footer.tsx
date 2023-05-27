315ad7d0a59e9b638f9a486b49ac5202dc74c6d0
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2023-05-27 09:13:24 +0800

    Fix content page responsiveness
    fix:
    - Fix content page responsiveness

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,10 @@ import React from "react";
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-100 bottom-0 fs-4 fw-semibold text-center py-3 bg-dark bg-opacity-75 text-white">
+    <footer
+      id="footer"
+      className="w-100 bottom-0 fs-6 fw-semibold text-center py-3 bg-dark bg-opacity-75 text-white position-absolute"
+    >
       Atur Belanja Copyright @ {currentYear}
     </footer>
   );
